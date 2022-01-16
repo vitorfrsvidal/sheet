38e89a0f20957770bc9122be5d57905edfdbbcb4
Vitor Vidal <vitorfrsvidal@gmail.com>
2022-01-16 19:17:14 -0300

    Added hour, minute and second to git upload script

--- bin/git_upload_sheet.sh
+++ bin/git_upload_sheet.sh
@@ -5,7 +5,7 @@ SHEET=$(ls | grep "^[0-9][0-9][0-9][0-9]$")
 if [ -n "${SHEET}" ] && git status | grep "modified:" | grep "${SHEET}" ; then
 
 	git add ${SHEET}
-	git commit -m "$(date +%d-%m-%Y)"
+	git commit -m "$(date +%d-%m-%Y_%H:%M:%S)"
 	git push -u origin main
 
 fi
