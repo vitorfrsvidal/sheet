32047931c3c2aac29e82c2fdac0588ab21b73948
Vitor Vidal <vitorfrsvidal@gmail.com>
2022-01-16 17:56:35 -0300

    Modified git upload script

--- bin/git_upload_sheet.sh
+++ bin/git_upload_sheet.sh
@@ -6,7 +6,7 @@ if [ -n "${SHEET}" ] && git status | grep "modified:" | grep "${SHEET}" ; then
 
 	git add ${SHEET}
 	git commit -m "$(date +%d-%m-%Y)"
-	git push -u origin master
+	git push -u origin main
 
 fi
 
